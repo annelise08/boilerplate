134bfbad7ae4ff7bb78446d9be0ec016218cb426
Anne-lise Emig <anneliseemig@gmail.com>
2023-04-14 09:13:08 -0500

    server created and connected

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: process.env.NODE_ENV,
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, '/dist'),
